4a9cd27d0f185853f84bef879a02f1b88a095cbe
PiotrJerzy13 <138906583+PiotrJerzy13@users.noreply.github.com>
2026-05-05 21:30:09 +0200

    initial project setup

---
+++ src/main/java/com/solanabot/solana_bot/SolanaBotApplication.java
@@ -0,0 +1,13 @@
+package com.solanabot.solana_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolanaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolanaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solanabot/solana_bot/SolanaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solanabot.solana_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolanaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
